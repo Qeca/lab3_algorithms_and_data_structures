0dc80df0b098ac4f44603c9dd84b832694db74f4
4219 Sharonov D.V <dima.sharonov.2004@mail.ru>
2023-10-21 23:37:13 +0300

    Добавил бенчмарки

--- bench.py
+++ bench.py
@@ -1,27 +1,26 @@
-from RBTree import RBTree, Student
+from RBTree import RBTree, Car
 import timeit
 
-students: list[Student] = [
-    Student("Шаронов", "Дмитрий", "Вадимович", "4219", 2, 4.33),
-    Student("Иванов", "Иван", "Иванович", "4219", 2, 4.33),
-    Student("Петров", "Петр", "Петрович", "4219", 3, 4.11),
-    Student("Сидоров", "Андрей", "Васильевич", "4220", 2, 4.85),
-    Student("Козлова", "Мария", "Викторовна", "4221", 1, 4.75),
-    Student("Григорьева", "Екатерина", "Александровна", "4220", 3, 4.21),
-    Student("Смирнов", "Алексей", "Николаевич", "4218", 4, 4.02),
-    Student("Михайлов", "Денис", "Сергеевич", "4218", 3, 4.56),
-    Student("Зайцев", "Олег", "Валентинович", "4221", 2, 4.01),
-    Student("Лебедева", "Анна", "Андреевна", "4222", 1, 4.89),
-]
+cars: list[Car] = [
+        Car("W0999876532YYY234", "BMW", 1673, 123.33, 5000000),
+        Car("W1234567890ABCDEF", "Audi", 1900, 145.75, 6000000),
+        Car("W9876543210ABCXYZ", "Mercedes", 1750, 135.50, 5500000),
+        Car("W7777777777XYZ123", "Lamborghini", 1800, 200.25, 8000000),
+        Car("W5555555555LMN456", "Ferrari", 1650, 175.60, 7500000),
+        Car("W4444444444PQR789", "Porsche", 1600, 155.25, 7000000),
+        Car("W8888888888JKL321", "Jaguar", 1700, 130.75, 6500000),
+        Car("W6666666666QWE987", "Tesla", 1550, 190.30, 6500000),
+        Car("W2222222222MNO654", "Ford", 1500, 125.40, 6000000)
+    ]
 tree: RBTree = RBTree()
 
-for it in students:
+for it in cars:
     tree.insert(it)
 start_time = timeit.default_timer()
 tree.print_tree()
 print(timeit.default_timer() - start_time)
 
 start_time = timeit.default_timer()
-tree.remove(4.33)
+tree.is_balanced()
 print(timeit.default_timer() - start_time)
 tree.print_tree()
